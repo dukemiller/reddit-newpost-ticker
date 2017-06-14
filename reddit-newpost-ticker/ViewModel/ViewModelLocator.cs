e27d5017e4773726fd1b7a3beb5ce6d3ec7fb4a3
dukemiller <dukemiller928@hotmail.com>
2017-06-13 18:28:06 -0700

    Remove more boilerplate comments

--- reddit-newpost-ticker/ViewModel/ViewModelLocator.cs
+++ reddit-newpost-ticker/ViewModel/ViewModelLocator.cs
@@ -5,15 +5,8 @@ using reddit_newpost_ticker.Service;
 
 namespace reddit_newpost_ticker.ViewModel
 {
-    /// <summary>
-    /// This class contains static references to all the view models in the
-    /// application and provides an entry point for the bindings.
-    /// </summary>
     public class ViewModelLocator
     {
-        /// <summary>
-        /// Initializes a new instance of the ViewModelLocator class.
-        /// </summary>
         public ViewModelLocator()
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
