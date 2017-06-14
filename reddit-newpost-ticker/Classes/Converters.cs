16607007fd2caa4272bcd988677367a7e540519f
dukemiller <dukemiller928@hotmail.com>
2017-06-14 12:39:16 -0700

    Add xaml converter classes

---
+++ reddit-newpost-ticker/Classes/Converters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Web;
+using System.Windows.Data;
+
+namespace reddit_newpost_ticker.Classes
+{
+    public class HtmlDecodeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return HttpUtility.HtmlDecode(System.Convert.ToString(value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RedditConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToString(value).ToLower().Contains("reddit");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
